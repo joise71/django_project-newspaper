5f75f24463f02073c1c2ceae5dfa8544b60a16ed
Viacheslav <vyacheslav.tkachenko71@gmail.com>
2023-02-03 04:36:20 +0100

    added LoginRequired to delete,create,update functions

--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.urls import path, reverse_lazy
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 from . import models
 
 
@@ -14,22 +15,25 @@ class ArticleDetailView(DetailView):
     template_name = 'article_detail.html'
 
 
-class ArticleUpdateView(UpdateView):
+class ArticleUpdateView(LoginRequiredMixin, UpdateView):
     model = models.Article
     fields = ['title', 'body']
     template_name = 'article_update.html'
+    login_url = 'login'
 
 
-class ArticleDeleteView(DeleteView):
+class ArticleDeleteView(LoginRequiredMixin, DeleteView):
     model = models.Article
     template_name = 'article_delete.html'
     success_url = reverse_lazy('article_list')
+    login_url = 'login'
 
 
-class ArticleCreateView(CreateView):
+class ArticleCreateView(LoginRequiredMixin, CreateView):
     model = models.Article
     template_name = 'article_create.html'
     fields = ['title', 'body', 'author']
+    login_url = 'login'
 
     def form_valid(self, form):
         form.instance.author = self.request.user
